d1122ddf85695f190c63f3a4bfe8a237a54e5859
lenutaionut <ionutlenuta96@gmail.com>
2023-01-29 23:29:07 +0200

    Formatt date and time using moment

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  public currentDate = Date.now();
+  public date = Date.now();
+  public currentDate = moment(this.date).format("MM/DD/yyyy hh:mm");
 
   constructor() { }
 
